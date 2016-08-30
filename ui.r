2fe24e5b6108b8221cb43ab39f436507b069c289
Bipin Pandey <pandeybipin007@gmail.com>
2016-08-30 11:18:52 -0400

    Update ui.r

--- ui.r
+++ ui.r
@@ -38,16 +38,9 @@ shinyUI(
                 tabPanel("Dly_Rtn_AAPL", tableOutput("out9")),
                 
                 #Tab for ploting the daily return of all 3 stocks 
-                tabPanel("Plots", plotOutput("out2")), 
-                
-                #Tab for displaying the cluster plot grouped into 2
-                tabPanel("Cluster_Plot", plotOutput("out3"))
-            ),
-            # It displays the logic that should be used to find the mismatch between the dates
-            column(4,
-                   textOutput("text1")
+                tabPanel("Plots", plotOutput("out2"))
+            )
             )
         )
     )
     
-)
